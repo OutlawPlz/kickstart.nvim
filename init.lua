deecbb11550a249ffaf019824f86bfcba6027c50
Matteo Palazzo <matteo.palazzo@double-you.it>
2026-08-17 14:51:17 +0200

    Removes mason

--- init.lua
+++ init.lua
@@ -613,7 +613,7 @@ end
 
 -- ============================================================
 -- SECTION 6: LSP
--- LSP keymaps, server configuration, Mason tools installations
+-- LSP keymaps and server configuration
 -- ============================================================
 do
   -- [[ LSP Configuration ]]
@@ -637,7 +637,7 @@ do
   --  - and more!
   --
   -- Thus, Language Servers are external tools that must be installed separately from
-  -- Neovim. This is where `mason` and related plugins come into play.
+  -- Neovim.
   --
   -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
   -- and elegantly composed help section, `:help lsp-vs-treesitter`
@@ -715,7 +715,7 @@ do
   })
 
   -- Enable the following language servers
-  --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+  --  Feel free to add/remove any LSPs that you have installed.
   --  See `:help lsp-config` for information about keys and how to configure
   ---@type table<string, vim.lsp.Config>
   local servers = {
@@ -731,8 +731,6 @@ do
     ts_ls = {},
 
     intelephense = {},
-
-    stylua = {}, -- Used to format Lua code
 
     -- Special Lua Config, as recommended by neovim help docs
     lua_ls = {
@@ -767,42 +765,15 @@ do
     },
   }
 
-  vim.pack.add {
-    gh 'neovim/nvim-lspconfig',
-    gh 'mason-org/mason.nvim',
-    gh 'mason-org/mason-lspconfig.nvim',
-    gh 'WhoIsSethDaniel/mason-tool-installer.nvim',
-  }
-
-  -- Automatically install LSPs and related tools to stdpath for Neovim
-  require('mason').setup {}
-
-  -- Translates between nvim-lspconfig server names and mason.nvim package names (e.g. lua_ls <-> lua-language-server)
-  require('mason-lspconfig').setup {
-    automatic_enable = false, -- Change this to true if you want to automatically enable servers that are installed manually (e.g. via :Mason / :MasonInstall)
-  }
-
-  -- Ensure the servers and tools above are installed
-  --
-  -- To check the current status of installed tools and/or manually install
-  -- other tools, you can run
-  --    :Mason
-  --
-  -- You can press `g?` for help in this menu.
-  local ensure_installed = vim.tbl_keys(servers or {})
-  vim.list_extend(ensure_installed, {
-    -- You can add other tools here that you want Mason to install
-  })
-
-  require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+  vim.pack.add { gh 'neovim/nvim-lspconfig' }
 
   for name, server in pairs(servers) do
     vim.lsp.config(name, server)
     vim.lsp.enable(name)
   end
 
-  -- Laravel's official language server. It is distributed via Composer rather
-  -- than Mason, so install it with `composer global require laravel/lsp`.
+  -- Laravel's official language server. Install it with
+  -- `composer global require laravel/lsp`.
   --  See https://github.com/laravel/lsp
   if vim.fn.executable 'laravel-lsp' == 1 then
     vim.lsp.config('laravel_lsp', {
@@ -843,6 +814,7 @@ do
     },
     -- You can also specify external formatters in here.
     formatters_by_ft = {
+      lua = { 'stylua' },
       -- rust = { 'rustfmt' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
